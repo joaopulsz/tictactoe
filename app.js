2da7acb9d9de1a36ac2ca6c83208545682b4e25f
joaopulsz <101181633+joaopulsz@users.noreply.github.com>
2022-05-26 16:54:36 +0100

    uploading app.js file
    created initial game logic

--- app.js
+++ app.js
@@ -1,5 +1,52 @@
-// test
-//test
+const squares = document.querySelectorAll('.square');
 
+let lastMove = 'x';
 
-const square = 0;
+const makeAMove = (square) => {
+
+    if (lastMove === 'x') {
+        square.innerText = 'o';
+        lastMove = 'o';
+    } else if (lastMove === 'o') {
+        square.innerText = 'x';
+        lastMove = 'x';
+    }
+
+    if (squares[0].innerText === 'x' && squares[1].innerText === 'x' && squares[2].innerText === 'x') {
+        console.log('cross wins');
+    } else if (squares[3].innerText === 'x' && squares[4].innerText === 'x' && squares[5].innerText === 'x') {
+        console.log('cross wins');
+    } else if (squares[6].innerText === 'x' && squares[7].innerText === 'x' && squares[8].innerText === 'x') {
+        console.log('cross wins');
+    } else if (squares[1].innerText === 'x' && squares[4].innerText === 'x' && squares[7].innerText === 'x') {
+        console.log('cross wins');
+    } else if (squares[2].innerText === 'x' && squares[5].innerText === 'x' && squares[8].innerText === 'x') {
+        console.log('cross wins');
+    } else if (squares[0].innerText === 'x' && squares[4].innerText === 'x' && squares[8].innerText === 'x') {
+        console.log('cross wins');
+    } else if (squares[2].innerText === 'x' && squares[4].innerText === 'x' && squares[6].innerText === 'x') {
+        console.log('cross wins');
+    } else if (squares[0].innerText === 'o' && squares[1].innerText === 'o' && squares[2].innerText === 'o') {
+        console.log('circle wins');
+    } else if (squares[3].innerText === 'o' && squares[4].innerText === 'o' && squares[5].innerText === 'o') {
+        console.log('circle wins');
+    } else if (squares[6].innerText === 'o' && squares[7].innerText === 'o' && squares[8].innerText === 'o') {
+        console.log('circle wins');
+    } else if (squares[1].innerText === 'o' && squares[4].innerText === 'o' && squares[7].innerText === 'o') {
+        console.log('circle wins');
+    } else if (squares[2].innerText === 'o' && squares[5].innerText === 'o' && squares[8].innerText === 'o') {
+        console.log('circle wins');
+    } else if (squares[0].innerText === 'o' && squares[4].innerText === 'o' && squares[8].innerText === 'o') {
+        console.log('circle wins');
+    } else if (squares[2].innerText === 'o' && squares[4].innerText === 'o' && squares[6].innerText === 'o') {
+        console.log('circle wins');
+    }
+
+}
+
+squares.forEach(square => {
+    square.addEventListener('click', function handler() {
+        makeAMove(square);
+        this.removeEventListener('click', handler);
+    });
+})
